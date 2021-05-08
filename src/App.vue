22fd5df500efe5b38a790ac579e7b3e81687c171
Shunsuke Takagi (MacBook Pro) <takagishunsuke1129@gmail.com>
2021-05-08 11:31:13 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld v-if="isLoggedIn" v-bind:msg="'Hello, ' + userName"/>
+  <button class="btn-square" v-if="!isInClient" @click="logout">ログアウト</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import liff  from "@line/liff";
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      userName: undefined,
+      isInClient: undefined,
+      isLoggedIn: false,
+    }
+  },
+  created() {
+    liff
+      .init({
+        liffId: process.env.VUE_APP_LIFF_ID
+      })
+      .then(() => {
+        this.isLoggedIn = liff.isLoggedIn();
+        if (!liff.isInClient() && !liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          this.isInClient = liff.isInClient();
+          liff.getProfile()
+            .then(profile => {
+              this.userName = profile.displayName;
+            })
+        }
+      })
+  },
+  methods: {
+    logout: function () {
+      if (liff.isLoggedIn()) {
+        liff.logout();
+        location.reload();
+      }
+    }
   }
 }
 </script>
@@ -24,5 +57,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn-square {
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #42b983;
+  color: #FFF;
+  border-bottom: solid 4px #627295;
+  border-radius: 3px;
+}
+.btn-square:active {
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);
+  border-bottom: none;
 }
 </style>
